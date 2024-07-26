4d72a830819b7b8819f0ef537dda930a0044a362
Michael Carlin <michaelcarlin076@gmail.com>
2024-07-26 10:07:52 +0100

    Implement Redux-Persist to persist data in state after refresh.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,51 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import productSlice from "./productSlice"; // Adjust the path if necessary
 import basketSlice from "./basketSlice"; // Adjust the path if necessary
 
-const store = configureStore({
-  reducer: {
-    products: productSlice,
-    basket: basketSlice,
-  },
+// Persist configuration
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+// Combine your slices
+const rootReducer = combineReducers({
+  products: productSlice,
+  basket: basketSlice,
 });
+
+// Persist the combined reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// const store = configureStore({
+//   reducer: {
+//     products: productSlice,
+//     basket: basketSlice,
+//   },
+// });
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export type State = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
